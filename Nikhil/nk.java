d5155b01564f52608de7fda4a0b6fd9dab57ea47
Nikhil Patil <nickpatil1010@gmail.com>
2025-05-24 22:09:19 +0530

    program

---
+++ Nikhil/nk.java
@@ -0,0 +1,13 @@
+public class nk{
+    public static void main (String[] args){
+        int sum=5;
+        if(sum>3)
+        {
+            System.out.println("ture");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
