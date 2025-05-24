ebd4d1dc4b052ad8ae4db68dd52f8aa02eef271a
Nikhil Patil <nickpatil1010@gmail.com>
2025-05-24 23:39:00 +0530

    dowhile use

---
+++ Nikhil/dowhileloop.java
@@ -0,0 +1,9 @@
+public class dowhileloop {
+    public static void main(String[] agrs){
+        int i =1;
+        do { 
+            System.out.println("count" +i);
+            i++;
+        } while (i<5);
+    }
+}
